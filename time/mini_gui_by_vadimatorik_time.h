635438b8b9d0f944e020a3b7f2e98d8ccacc5262
Vadimatorik <deryabkinda@mail.ru>
2017-07-12 11:47:21 +0700

    Убрал stdio.

--- time/mini_gui_by_vadimatorik_time.h
+++ time/mini_gui_by_vadimatorik_time.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdio.h>
+#include <stdint.h>
 
 // Методы для работы с временем минуты:секунды.
 void mini_gui_by_vadimatorik_convert_sec_to_string ( uint32_t sec, char* const string );
